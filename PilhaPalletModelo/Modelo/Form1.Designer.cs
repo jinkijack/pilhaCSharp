3fc4bfc80e66061f0c05a36a5edf493f0117c4af
Tiago Mateus Carvalho <63086447+jinkijack@users.noreply.github.com>
2021-05-17 01:32:16 -0300

    Add files via upload

---
+++ PilhaPalletModelo/Modelo/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Modelo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listPallets = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtQtd = new System.Windows.Forms.TextBox();
+            this.txtProd = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(159)))));
+            this.panel1.Controls.Add(this.btnSobre);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 612);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.FlatAppearance.BorderSize = 0;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSobre.ForeColor = System.Drawing.Color.White;
+            this.btnSobre.Location = new System.Drawing.Point(0, 0);
+            this.btnSobre.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(141, 90);
+            this.btnSobre.TabIndex = 9;
+            this.btnSobre.Text = "Sobre";
+            this.btnSobre.UseVisualStyleBackColor = true;
+            this.btnSobre.Click += new System.EventHandler(this.BtnSobre_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(0)))), ((int)(((byte)(238)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.btnFechar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(141, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1209, 154);
+            this.panel2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Modelo.Properties.Resources.sinfo;
+            this.pictureBox1.Location = new System.Drawing.Point(228, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(539, 181);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.FlatAppearance.BorderSize = 0;
+            this.btnFechar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFechar.Image = global::Modelo.Properties.Resources.close;
+            this.btnFechar.Location = new System.Drawing.Point(1079, 0);
+            this.btnFechar.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(156, 109);
+            this.btnFechar.TabIndex = 12;
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(312, 172);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(567, 61);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sistema de Controle de Pallets";
+            // 
+            // listPallets
+            // 
+            this.listPallets.FormattingEnabled = true;
+            this.listPallets.ItemHeight = 28;
+            this.listPallets.Location = new System.Drawing.Point(1007, 287);
+            this.listPallets.Margin = new System.Windows.Forms.Padding(4);
+            this.listPallets.Name = "listPallets";
+            this.listPallets.Size = new System.Drawing.Size(338, 228);
+            this.listPallets.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Produto:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 118);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Quantidade:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtQtd);
+            this.groupBox1.Controls.Add(this.txtProd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(174, 272);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(802, 264);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operar Pallet";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Modelo.Properties.Resources.minus_square_outlined_button1;
+            this.button1.Location = new System.Drawing.Point(463, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 70);
+            this.button1.TabIndex = 11;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Image = global::Modelo.Properties.Resources.plus1;
+            this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(355, 164);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(79, 70);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtQtd
+            // 
+            this.txtQtd.Location = new System.Drawing.Point(167, 110);
+            this.txtQtd.Name = "txtQtd";
+            this.txtQtd.Size = new System.Drawing.Size(599, 36);
+            this.txtQtd.TabIndex = 9;
+            // 
+            // txtProd
+            // 
+            this.txtProd.Location = new System.Drawing.Point(167, 52);
+            this.txtProd.Name = "txtProd";
+            this.txtProd.Size = new System.Drawing.Size(599, 36);
+            this.txtProd.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1350, 612);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listPallets);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSobre;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listPallets;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtQtd;
+        private System.Windows.Forms.TextBox txtProd;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PilhaPalletModelo/Modelo/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Modelo
+{
+    public partial class Form1 : Form
+    {
+        Stack<Pallet> pilha = new Stack<Pallet>(); //global
+
+        public Form1()
+        {
+            InitializeComponent();
+            carregar();
+        }
+
+        void salvar()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("pallets.txt", FileMode.Create)))
+            {
+                writer.Write(pilha.Count());
+                foreach (Pallet p in pilha)
+                {
+                    writer.Write(p.Nome);
+                    writer.Write(p.Quantidade);
+                }
+            }
+        }
+
+        void carregar()
+        {
+            if (File.Exists("pallets.txt"))
+            {
+                Stack<Pallet> aux = new Stack<Pallet>();
+                using (BinaryReader reader = new BinaryReader(File.Open("pallets.txt", FileMode.Open)))
+                {
+                    int qtd = reader.ReadInt32();
+                    for (int i = 0; i < qtd; i++) { 
+                    Pallet p = new Pallet();
+                    p.Nome = reader.ReadString();
+                    p.Quantidade = reader.ReadInt32();
+                    aux.Push(p);
+                  }// fim for
+                }
+                foreach (Pallet p in aux)
+                {
+                    pilha.Push(p);
+                }// fim foreach
+                mostraPilha();
+            }
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            salvar();
+            this.Close();
+        }
+
+        private void BtnSobre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void mostraPilha()
+        {
+            listPallets.Items.Clear();
+            foreach (Pallet p in pilha)
+            {
+                listPallets.Items.Add(p.Nome + ": "+p.Quantidade);
+            }
+        }
+
+        void limpar()
+        {
+            txtProd.Clear();
+            txtQtd.Clear();
+            txtProd.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Pallet p = new Pallet();
+            p.Nome = txtProd.Text;
+            p.Quantidade = Convert.ToInt32(txtQtd.Text);
+            pilha.Push(p);
+            mostraPilha();
+            limpar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int quantia = Convert.ToInt32(txtQtd.Text);
+            
+            Stack<Pallet> aux = new Stack<Pallet>();
+            while (pilha.Count > 0)
+            {
+                Pallet p = new Pallet();
+
+                p = pilha.Peek(); // pegando o pallet que esta no topo
+                
+                if (p.Nome.Equals(txtProd.Text))
+                {
+                        p.Quantidade = p.Quantidade - quantia;
+                        // realizar os procedimentos de remoção 
+                        //testar se vai remover total ou parcial
+                    if (p.Quantidade <= 0) {
+                        pilha.Pop();
+                        break;
+                    }
+                    break;
+                    // realizar os procedimentos de remoção 
+                    //testar se vai remover total ou parcial
+                }
+                else {
+                    aux.Push(pilha.Pop());
+                    // passar o pallet para a pilha auxiliar 
+                }
+            }// fim while
+            
+            // depois de remover tem que devolver do aux para a pilha
+            foreach (Pallet p in aux) {
+                pilha.Push(p);
+                //retonar os valores da aux para a pilha principal
+            }// fim foreach
+            
+            mostraPilha();
+            limpar();
+        }
+    }
+}
